fadc47e4d074f4f85b47dde198c61806240ee73c
andrewpollack <andrewpkq@gmail.com>
2024-12-07 17:58:52 -0800

    Add template

---
+++ 2024/template/day.go
@@ -0,0 +1,21 @@
+package dayXX
+
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+func Main() {
+	logger := log.New(os.Stdout, "dayXX: ", 0)
+	inputFile := flag.String("inputFile5", "data/dayXX.txt", "Input file for day")
+	flag.Parse()
+
+	data, err := os.ReadFile(*inputFile)
+	if err != nil {
+		logger.Fatalf("error reading file %q: %v", *inputFile, err)
+	}
+
+	_ = string(data)
+	// strData := string(data)
+}
